7be09c93e4c52ffa1662bdd2fbb10325bd83aa7f
Yee Chin, Chiam <phathetique@gmail.com>
2017-10-06 01:55:05 -0400

    Initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA7uEQKxfRbiwMuX7GkLWB01mwtP_IBiAw',
+  authDomain: 'wave-2-us.firebaseapp.com',
+  databaseURL: 'https://wave-2-us.firebaseio.com',
+  projectId: 'wave-2-us',
+  storageBucket: 'wave-2-us.appspot.com',
+  messagingSenderId: '1080589429686'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
